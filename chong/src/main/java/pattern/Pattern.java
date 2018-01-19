c4673a93fddf3789479aa6abe0622affdcdcd651
tolu <1192317365@qq.com>
2018-01-19 20:41:46 +0800

    chong

---
+++ chong/src/main/java/pattern/Pattern.java
@@ -0,0 +1,12 @@
+package pattern;
+
+import ocr.OCR;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * Created by 618 on 2018/1/12.
+ */
+public interface Pattern {
+    String run() throws UnsupportedEncodingException;
+}
